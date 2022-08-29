ecfc252203985d2ad0786abc07b27e60bdda7002
TimT <timtuvestam@hotmail.se>
2022-08-29 10:20:31 +0200

    Visat If-sats

---
+++ Visning/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+        string s = "Tim";
+
+        if(s == "Tim")
+                Console.WriteLine("Personen heter Tim");
+        else
+                Console.WriteLine("Personsen heter inte TIm");
+
